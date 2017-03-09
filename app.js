d35c191abd6f483a8d63c154922ab833283d544a
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-09 17:13:09 +0100

    if et else if pour nombre aleatoire

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ console.log  (solution);
 
  var response= prompt ("choissisez un chiffre entre 0 et 3");
 
- if (solution > response)
+ if (solution > response){
  	console.log ("tu es en dessous");
- else if (solution < response)
+ }
+ else if (solution < response){
  	console.log ("tu es au dessus");
- else 
- 	console.log ("tu a gagner");
+ }
+ else {
+ 	console.log ("tu a gagner");}
