93f903005719c7608fda1b594da282ee96e9d848
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-09 16:56:59 +0100

    conditions if else if

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ var solution = Math.floor(Math.random()*4);
 console.log  (solution);
 
  var response= prompt ("choissisez un chiffre entre 0 et 3");
+
+ if (solution > response)
+ 	console.log ("tu es en dessous");
+ else if (solution < response)
+ 	console.log ("tu es au dessus");
+ else 
+ 	console.log ("tu a gagner");
