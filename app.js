30af79d98a7e5298d831eff7ac6df583410671e6
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-10 10:45:25 +0100

    1ere boucle for

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var solution = Math.floor(Math.random()*4);
 console.log  (solution);
 
- var response= prompt ("choissisez un chiffre entre 0 et 3");
+for(var i=0; i<3; i++){
+ 	var response= prompt ("choissisez un chiffre entre 0 et 3");
 
- if (solution > response){
- 	console.log ("tu es en dessous");
+
+	 if (solution > response){
+	 	console.log ("tu es en dessous");
+	 }
+	 else if (solution < response){
+	 	console.log ("tu es au dessus");
+	 }
+	 else {
+	 	console.log ("tu a gagner");break;}
  }
- else if (solution < response){
- 	console.log ("tu es au dessus");
- }
- else {
- 	console.log ("tu a gagner");}
+
+ 	
